3c41ab2c35b6139255fb5fb4ad4901e68e16909e
Mathieu 'Heinz' Agostini <mathieu@42nice.fr>
2025-04-16 16:22:00 +0200

    [BACKEND/DB] Added a sql database, with init script, makefile rule to setup test database, and started some go resolvers to interact with it

---
+++ backend/srcs/database/define.go
@@ -0,0 +1,78 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+type Role struct {
+	ID    string `json:"id" example:"01HZ0MMK4S6VQW4WPHB6NZ7R7X"`
+	Name  string `json:"name" example:"captain-hook"`
+	Color string `json:"color" example:"0xFF00FF"`
+}
+
+type Module struct {
+	ID            string    `json:"id" example:"01HZ0MMK4S6VQW4WPHB6NZ7R7X"`
+	Name          string    `json:"name" example:"captain-hook"`
+	Version       string    `json:"version" example:"1.2"`
+	Status        string    `json:"status" example:"enabled"`
+	URL           string    `json:"url" example:"https://github.com/some-user/some-repo"`
+	LatestVersion string    `json:"lastestVersion" example:"1.7"`
+	LateCommits   int       `json:"lateCommits" example:"2"`
+	LastUpdate    time.Time `json:"lastUpdate" example:"2025-02-18T15:00:00Z"`
+}
+
+type User struct {
+	ID        string    `json:"id" example:"01HZ0MMK4S6VQW4WPHB6NZ7R7X"`
+	FtLogin   string    `json:"login" example:"heinz"`
+	FtID      string    `json:"ftId" example:"1492"`
+	FtIsStaff bool      `json:"ftIsStaff" example:"true"`
+	PhotoURL  string    `json:"url" example:"https://intra.42.fr/some-login/some-id"`
+	LastSeen  time.Time `json:"lastUpdate" example:"2025-02-18T15:00:00Z"`
+	IsStaff   bool      `json:"isStaff" example:"true"`
+}
+
+type RolePatch struct {
+	ID    string  `json:"id" example:"01HZ0MMK4S6VQW4WPHB6NZ7R7X"`
+	Name  *string `json:"name" example:"captain-hook"`
+	Color *string `json:"color" example:"0xFF00FF"`
+}
+
+type ModulePatch struct {
+	ID            string     `json:"id" example:"01HZ0MMK4S6VQW4WPHB6NZ7R7X"`
+	Name          *string    `json:"name" example:"captain-hook"`
+	Version       *string    `json:"version" example:"1.2"`
+	Status        *string    `json:"status" example:"enabled"`
+	URL           *string    `json:"url" example:"https://github.com/some-user/some-repo"`
+	LatestVersion *string    `json:"lastestVersion" example:"1.7"`
+	LateCommits   *int       `json:"lateCommits" example:"2"`
+	LastUpdate    *time.Time `json:"lastUpdate" example:"2025-02-18T15:00:00Z"`
+}
+
+type UserPatch struct {
+	ID        string     `json:"id" example:"01HZ0MMK4S6VQW4WPHB6NZ7R7X"`
+	FtLogin   *string    `json:"login" example:"heinz"`
+	FtID      *string    `json:"ftId" example:"1492"`
+	FtIsStaff *bool      `json:"ftIsStaff" example:"true"`
+	PhotoURL  *string    `json:"url" example:"https://intra.42.fr/some-login/some-id"`
+	LastSeen  *time.Time `json:"lastUpdate" example:"2025-02-18T15:00:00Z"`
+	IsStaff   *bool      `json:"isStaff" example:"true"`
+}
+
+var db *sql.DB
+
+func init() {
+	var err error
+	connStr := os.Getenv("DATABASE_URL")
+	if connStr == "" {
+		log.Fatal("DATABASE_URL is not set")
+	}
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Couldn't connect to database: ", err)
+	}
+}
---
+++ db/init.sql
@@ -0,0 +1,39 @@
+CREATE TABLE roles (
+  id TEXT PRIMARY KEY, -- role_ULID
+  name TEXT NOT NULL,
+  color TEXT NOT NULL
+);
+
+CREATE TABLE modules (
+  id TEXT PRIMARY KEY, -- module_ULID
+  name TEXT NOT NULL,
+  version TEXT,
+  status TEXT,
+  url TEXT,
+  latest_version TEXT,
+  late_commits INT,
+  last_update TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE users (
+  id TEXT PRIMARY KEY, -- user_ULID
+  ft_login TEXT NOT NULL,
+  ft_id TEXT NOT NULL,
+  ft_is_staff BOOLEAN NOT NULL,
+  photo_url TEXT NOT NULL,
+  last_seen TIMESTAMP WITH TIME ZONE NOT NULL,
+  is_staff BOOLEAN NOT NULL
+);
+
+-- join tables
+CREATE TABLE user_roles (
+  user_id TEXT REFERENCES users(id),
+  role_id TEXT REFERENCES roles(id),
+  PRIMARY KEY (user_id, role_id)
+);
+
+CREATE TABLE module_roles (
+  module_id TEXT REFERENCES modules(id),
+  role_id TEXT REFERENCES roles(id),
+  PRIMARY KEY (module_id, role_id)
+);
