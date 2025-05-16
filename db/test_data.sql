4900f7cdb638b3045537ba5f5acf6d6ecd3bf6b4
heinz <heinz@42nice.fr>
2025-05-16 14:50:34 +0200

    [DATABASE] Change database setup, so it has one schema, and templates used for tests

--- db/test_data.sql
+++ db/test_data.sql
@@ -1,9 +1,9 @@
 INSERT INTO users (id, ft_login, ft_id, ft_is_staff, photo_url, last_seen, is_staff)
 VALUES
-  ('user_01HZXYZDE0420', 'heinz',    '220393', true,  'https://cdn.intra.42.fr/users/e02ff39566a64af026f137b30c18c3e4/heinz.png',    '2025-04-24 12:00:00', true),
-  ('user_01HZXYZDE0430', 'ltcherep', '194037', false, 'https://cdn.intra.42.fr/users/2d49a08ed473a0705e7f7cfd5cd3ae72/ltcherep.jpg', '2002-04-16 12:00:00', false),
-  ('user_01HZXYZDE0440', 'tac',      '79125',  true,  'https://cdn.intra.42.fr/users/41714d1b596f06cc352f21791ee64096/tac.png',      '2003-04-16 12:00:00', true),
-  ('user_01HZXYZDE0450', 'rose',     '188547',  true, 'https://cdn.intra.42.fr/users/8d185c6e0587ce836f0185e05daa5b13/rose.jpg',     '2004-04-16 12:00:00', false),
+  ('user_01HZXYZDE0420', 'heinz',    '220393', true,  'https://cdn.intra.42.fr/users/e02ff39566a64af026f137b30c18c3e4/heinz.png',      '2025-04-24 12:00:00', true),
+  ('user_01HZXYZDE0430', 'ltcherep', '194037', false, 'https://cdn.intra.42.fr/users/2d49a08ed473a0705e7f7cfd5cd3ae72/ltcherep.jpg',   '2002-04-16 12:00:00', false),
+  ('user_01HZXYZDE0440', 'tac',      '79125',  true,  'https://cdn.intra.42.fr/users/41714d1b596f06cc352f21791ee64096/tac.png',        '2003-04-16 12:00:00', true),
+  ('user_01HZXYZDE0450', 'rose',     '188547',  true, 'https://cdn.intra.42.fr/users/8d185c6e0587ce836f0185e05daa5b13/rose.jpg',       '2004-04-16 12:00:00', false),
   ('user_01HZXYZDE0451', 'SomeUser', '188548',  true, 'https://cdn.intra.42.fr/users/43445ac80da38e73e2af06b5897339fd/anissa.jpg',     '1999-04-18 12:00:00', false),
   ('user_01HZXYZDE0452', 'SomeUser', '188549',  true, 'https://cdn.intra.42.fr/users/43445ac80da38e73e2af06b5897339fd/anissa.jpg',     '1999-04-18 12:00:00', false),
   ('user_01HZXYZDE0453', 'SomeUser', '188550',  true, 'https://cdn.intra.42.fr/users/43445ac80da38e73e2af06b5897339fd/anissa.jpg',     '1999-04-18 12:00:00', false),
@@ -53,25 +53,52 @@ VALUES
 
 INSERT INTO modules (id, name, version, status, url, latest_version, late_commits, last_update)
 VALUES
-  ('module_01HZXYZDE0420', 'captain-hook', '1.2', 'enabled', 'https://github.com/42nice/captain-hook', '1.7', 5, '2025-04-16 12:00:00'),
-  ('module_01HZXYZDE0430', 'adm-stud', '1.5', 'enabled', 'https://github.com/42nice/adm-stud', '1.5', 0, '2025-04-16 12:00:00'),
-  ('module_01HZXYZDE0440', 'adm-manager', '1.0', 'enabled', 'https://github.com/42nice/adm-manager', '1.0', 0, '2025-04-16 12:00:00'),
-  ('module_01HZXYZDE0450', 'student-info', '1.8', 'enabled', 'https://github.com/42nice/student-info', '1.9', 1, '2025-04-16 12:00:00');
-
+  ('module_01HZXYZDE0420', 'captain-hook',    '1.2', 'enabled', 'https://github.com/42nice/captain-hook',    '1.7', 5, '2025-04-16 12:00:00+02'),
+  ('module_01HZXYZDE0430', 'adm-stud',        '1.5', 'enabled', 'https://github.com/42nice/adm-stud',        '1.5', 0, '2025-04-16 12:00:00+02'),
+  ('module_01HZXYZDE0440', 'adm-manager',     '1.0', 'enabled', 'https://github.com/42nice/adm-manager',     '1.0', 0, '2025-04-16 12:00:00+02'),
+  ('module_01HZXYZDE0450', 'student-info',    '1.8', 'enabled', 'https://github.com/42nice/student-info',    '1.9', 1, '2025-04-16 12:00:00+02'),
+  ('module_01HZXYZDE0460', 'role-manager',    '1.0', 'enabled', 'https://github.com/42nice/role-manager',    '1.0', 0, '2025-04-20 12:00:00+02'),
+  ('module_01HZXYZDE0461', 'role-editor',     '1.1', 'enabled', 'https://github.com/42nice/role-editor',     '1.1', 2, '2025-04-20 12:00:00+02'),
+  ('module_01HZXYZDE0462', 'support-tool',    '2.0', 'enabled', 'https://github.com/42nice/support-tool',    '2.0', 1, '2025-04-20 12:00:00+02'),
+  ('module_01HZXYZDE0463', 'analytics',       '3.2', 'enabled', 'https://github.com/42nice/analytics',       '3.2', 4, '2025-04-20 12:00:00+02'),
+  ('module_01HZXYZDE0464', 'design-proto',    '0.9', 'enabled', 'https://github.com/42nice/design-proto',    '0.9', 0, '2025-04-20 12:00:00+02'),
+  ('module_01HZXYZDE0465', 'test-suite',      '5.4', 'enabled', 'https://github.com/42nice/test-suite',      '5.4', 3, '2025-04-20 12:00:00+02'),
+  ('module_01HZXYZDE0466', 'deploy-automate', '1.5', 'enabled', 'https://github.com/42nice/deploy-automate', '1.5', 0, '2025-04-20 12:00:00+02'),
+  ('module_01HZXYZDE0467', 'strategy-dash',   '4.0', 'enabled', 'https://github.com/42nice/strategy-dash',   '4.0', 2, '2025-04-20 12:00:00+02');
 
 INSERT INTO roles (id, name, color)
 VALUES
-  ('role_01HZXYZDE0420', 'Student', '0xFFCD70'),
-  ('role_01HZXYZDE0430', 'ADM', '0x69DE7A'),
-  ('role_01HZXYZDE0440', 'Pedago', '0x86B3E7'),
-  ('role_01HZXYZDE0450', 'Cluster', '0x69D2DE'),
-  ('role_01HZXYZDE0460', 'Stagiaire', '0x75FFF8'),
-  ('role_01HZXYZDE0470', 'IT', '0xF26363');
+  ('role_01HZXYZDE0420', 'Student',     '0xFFCD70'),
+  ('role_01HZXYZDE0430', 'ADM',         '0x69DE7A'),
+  ('role_01HZXYZDE0440', 'Pedago',      '0x86B3E7'),
+  ('role_01HZXYZDE0450', 'Cluster',     '0x69D2DE'),
+  ('role_01HZXYZDE0400', 'Stagiaire',   '0x75FFF8'),
+  ('role_01HZXYZDE0410', 'IT',          '0xF26363'),
+  ('role_01HZXYZDE0460', 'Reviewer',    '0xAAAAAA'),
+  ('role_01HZXYZDE0461', 'Editor',      '0xBBBBBB'),
+  ('role_01HZXYZDE0462', 'Manager',     '0xCCCCCC'),
+  ('role_01HZXYZDE0463', 'Support',     '0xDDDDDD'),
+  ('role_01HZXYZDE0464', 'Operator',    '0xEEEEEE'),
+  ('role_01HZXYZDE0465', 'Guest',       '0x111111'),
+  ('role_01HZXYZDE0466', 'Developer',   '0x222222'),
+  ('role_01HZXYZDE0467', 'Analyst',     '0x333333'),
+  ('role_01HZXYZDE0468', 'Designer',    '0x444444'),
+  ('role_01HZXYZDE0469', 'Tester',      '0x555555'),
+  ('role_01HZXYZDE0470', 'Maintainer',  '0x666666'),
+  ('role_01HZXYZDE0471', 'Contributor', '0x777777'),
+  ('role_01HZXYZDE0472', 'Owner',       '0x888888'),
+  ('role_01HZXYZDE0473', 'SuperAdmin',  '0x999999'),
+  ('role_01HZXYZDE0474', 'Moderator',   '0x123456'),
+  ('role_01HZXYZDE0475', 'Auditor',     '0x654321'),
+  ('role_01HZXYZDE0476', 'Architect',   '0xABCDEF'),
+  ('role_01HZXYZDE0477', 'Coordinator', '0xFEDCBA'),
+  ('role_01HZXYZDE0478', 'Planner',     '0x0F0F0F'),
+  ('role_01HZXYZDE0479', 'Strategist',  '0xF0F0F0');
 
 INSERT INTO user_roles (user_id, role_id)
 VALUES
   ('user_01HZXYZDE0420', 'role_01HZXYZDE0460'), -- heinz Stagiaire
-  ('user_01HZXYZDE0430', 'role_01HZXYZDE0420'), -- ltcherep student
+  ('user_01HZXYZDE0430', 'role_01HZXYZDE0420'), -- ltcherep Student
   ('user_01HZXYZDE0440', 'role_01HZXYZDE0440'), -- tac Pedago
   ('user_01HZXYZDE0440', 'role_01HZXYZDE0430'), -- tac ADM
   ('user_01HZXYZDE0440', 'role_01HZXYZDE0450'), -- tac Cluster
@@ -82,51 +109,51 @@ VALUES
   ('user_01HZXYZDE0460', 'role_01HZXYZDE0470'), -- Yoshi IT
   ('user_01HZXYZDE0450', 'role_01HZXYZDE0430'), -- Rose ADM
   ('user_01HZXYZDE0450', 'role_01HZXYZDE0450'), -- Rose Cluster
-  ('user_01HZXYZDE0451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_01HZXYZDE0452', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_01HZXYZDE0453', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_01HZXYZDE0454', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_01HZXYZDE0455', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_01HZXYZDE0456', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_01HZXYZDE0457', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_01HZXYZDE0458', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_01HZXYZDE0459', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_01HZXYZDE1451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_01HZXYZDE2451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_01HZXYZDE3451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_01HZXYZDE4451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_01HZXYZDE5451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_01HZXYZDE6451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_01HZXYZDE7451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_01HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_02HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_03HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_04HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_05HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_06HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_07HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_08HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_09HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_11HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_21HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_31HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_41HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_51HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_61HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_71HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_81HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_91HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_10HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_12HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_13HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_14HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_15HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_16HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_17HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_18HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_19HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_22HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser ADM
-  ('user_25HZXYZDE8451', 'role_01HZXYZDE0420'); -- SomeUser ADM
+  ('user_01HZXYZDE0451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_01HZXYZDE0452', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_01HZXYZDE0453', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_01HZXYZDE0454', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_01HZXYZDE0455', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_01HZXYZDE0456', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_01HZXYZDE0457', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_01HZXYZDE0458', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_01HZXYZDE0459', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_01HZXYZDE1451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_01HZXYZDE2451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_01HZXYZDE3451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_01HZXYZDE4451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_01HZXYZDE5451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_01HZXYZDE6451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_01HZXYZDE7451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_01HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_02HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_03HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_04HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_05HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_06HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_07HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_08HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_09HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_11HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_21HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_31HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_41HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_51HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_61HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_71HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_81HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_91HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_10HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_12HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_13HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_14HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_15HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_16HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_17HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_18HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_19HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_22HZXYZDE8451', 'role_01HZXYZDE0420'), -- SomeUser Student
+  ('user_25HZXYZDE8451', 'role_01HZXYZDE0420'); -- SomeUser Student
 
 
 INSERT INTO module_roles (module_id, role_id)
