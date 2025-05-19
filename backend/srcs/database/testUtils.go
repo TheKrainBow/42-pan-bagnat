685578e00da350b54b1e300593418046895418c9
Mathieu 'Heinz' Agostini <mathieu@42nice.fr>
2025-05-19 10:48:30 +0200

    [TESTS] Add helper to correctly print errors for modules

--- backend/srcs/database/testUtils.go
+++ backend/srcs/database/testUtils.go
@@ -3,10 +3,31 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 	"testing"
 
 	_ "github.com/lib/pq"
 )
+
+func formatModules(ms []Module) string {
+	var b strings.Builder
+	if ms == nil {
+		return "nil"
+	}
+	b.WriteString("[]Module{\n")
+	for _, m := range ms {
+		// extract the components of LastUpdate
+		y, mon, d := m.LastUpdate.Date()
+		h, min, sec := m.LastUpdate.Clock()
+		b.WriteString(fmt.Sprintf(
+			"\t{ID: %q, Name: %q, Version: %q, Status: %q, URL: %q, LatestVersion: %q, LateCommits: %d, LastUpdate: time.Date(%d, %d, %d, %d, %d, %d, 0, time.UTC)},\n",
+			m.ID, m.Name, m.Version, m.Status, m.URL, m.LatestVersion, m.LateCommits,
+			y, int(mon), d, h, min, sec,
+		))
+	}
+	b.WriteString("},")
+	return b.String()
+}
 
 func DropDatabase(t *testing.T, dbName string) {
 	_, err := mainDB.Exec(fmt.Sprintf("DROP DATABASE %s WITH (FORCE)", dbName))
@@ -16,14 +37,20 @@ func DropDatabase(t *testing.T, dbName string) {
 }
 
 func CreateAndPopulateDatabase(t *testing.T, dbName string, sqlFile string) *sql.DB {
-	_, err := mainDB.Exec(fmt.Sprintf("DROP DATABASE IF EXISTS %s WITH (FORCE)", dbName))
+	_, err := mainDB.Exec(fmt.Sprintf(
+		"DROP DATABASE IF EXISTS %s WITH (FORCE)",
+		dbName,
+	))
 	if err != nil {
-		t.Fatalf("failed to drop database %s: %v", dbName, err)
+		t.Fatalf("failed to drop test database %s: %v", dbName, err)
 	}
 
-	_, err = mainDB.Exec(fmt.Sprintf("CREATE DATABASE %s", dbName))
+	_, err = mainDB.Exec(fmt.Sprintf(
+		"CREATE DATABASE %s TEMPLATE schema_template",
+		dbName,
+	))
 	if err != nil {
-		t.Fatalf("failed to create database %s: %v", dbName, err)
+		t.Fatalf("failed to create test database %s: %v", dbName, err)
 	}
 
 	testDB, err = sql.Open("postgres", fmt.Sprintf("postgresql://admin:pw_admin@localhost/%s?sslmode=disable", dbName))
