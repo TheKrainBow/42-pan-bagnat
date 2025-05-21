74d93469e7cba5afb8a372bb08803d4550310c14
heinz <heinz@42nice.fr>
2025-05-21 12:33:14 +0200

    [BACK] Add omitempty in API, to not return empty arrays

--- backend/srcs/handlers/api/define.go
+++ backend/srcs/handlers/api/define.go
@@ -10,8 +10,8 @@ type Role struct {
 	ID      string   `json:"id" example:"role_01HZ0MMK4S6VQW4WPHB6NZ7R7X"`
 	Name    string   `json:"name" example:"IT"`
 	Color   string   `json:"color" example:"0xFF00FF"`
-	Users   []User   `json:"users"`
-	Modules []Module `json:"modules"`
+	Users   []User   `json:"users,omitempty"`
+	Modules []Module `json:"modules,omitempty"`
 }
 
 // Define the model for the API User Object
@@ -24,7 +24,7 @@ type User struct {
 	PhotoURL  string    `json:"ft_photo" example:"https://intra.42.fr/some-login/some-id"`
 	LastSeen  time.Time `json:"last_seen" example:"2025-02-18T15:00:00Z"`
 	IsStaff   bool      `json:"is_staff" example:"true"`
-	Roles     []Role    `json:"roles"`
+	Roles     []Role    `json:"roles,omitempty"`
 }
 
 // Define the model for the API Module response
@@ -38,7 +38,7 @@ type Module struct {
 	LatestVersion string       `json:"latest_version" example:"1.7"`
 	LateCommits   int          `json:"late_commits" example:"2"`
 	LastUpdate    time.Time    `json:"last_update" example:"2025-02-18T15:00:00Z"`
-	Roles         []Role       `json:"roles"`
+	Roles         []Role       `json:"roles,omitempty"`
 }
 
 const (
