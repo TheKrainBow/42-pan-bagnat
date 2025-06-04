6c291afeb91d8a56fbf971ba5b168f77e02a17c5
Mathieu 'Heinz' Agostini <mathieu@42nice.fr>
2025-06-04 11:30:50 +0200

    [FRONTEND/REFACTOR] Change how import are working, and improve ModuleSettings

---
+++ frontend/src/Pages/Modules/components/ModuleLogs.jsx
@@ -0,0 +1,13 @@
+// components/ModuleLogs.jsx
+import React from 'react';
+import './ModuleLogs.css';
+
+const ModuleLogs = () => {
+  return (
+    <pre className="log-box">
+      [Sample logs will be displayed here]
+    </pre>
+  );
+};
+
+export default ModuleLogs;
