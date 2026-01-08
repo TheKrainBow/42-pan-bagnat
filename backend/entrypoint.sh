4c37b97703aeabe5e295eb451b89029ac901449f
heinz <heinz@42nice.fr>
2026-01-08 18:13:13 +0100

    [BUG] Add know_host generator in backend/entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,5 +6,8 @@ if [ -d /data/repos ]; then
   chown -R app:app /data/repos
 fi
 
+ssh-keyscan github.com > ./known_hosts
+ssh-keyscan gitlab-world.42.fr >> ./known_hosts
+
 # exec your binary
 exec ./main "$@"
